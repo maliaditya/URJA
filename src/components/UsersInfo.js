75aa9228ff7e2d4a114e23335fc0e824db70da58
aditya <adityamali33@gmail.com>
2021-06-26 15:58:05 +0530

    a

--- src/components/UsersInfo.js
+++ src/components/UsersInfo.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
 import ProductCard from './ProductCard'
-import { MdFavoriteBorder } from 'react-icons/md'
 const UsersInfo = () => {
   return (
     <Wrapper className='content'>
